2b025af89feefb775cdb0d787b89e65862a21a28
Acer <kraubaevazanerke@gmail.com>
2026-02-04 16:31:03 +0500

    zhanekpanek

---
+++ src/test/java/com/example/onlineshoppingapi/OnlineShoppingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineshoppingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShoppingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
